da9850664540364f120e6577637231af47bfbb13
NehemieTuring <ewanenehemie30@gmail.com>
2026-05-12 17:39:48 +0100

    Nehemie 12/05/26

--- core/urls.py
+++ core/urls.py
@@ -6,9 +6,18 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/compta_matiere/', include('apps.comptabilite_matiere.urls')),
+    
+    # Authentification JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Documentation API (Swagger / OpenAPI)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
